a7f8bca52b901aad8a5281339cbeea341c750d12
MUHAMMAD IKBAL SOLAHUDIN <muhammad.ikbal_ti24@nusaputra.ac.id>
2025-05-05 10:57:04 +0700

    Add files via upload

---
+++ Sesi 9/Input angka angka.py
@@ -0,0 +1,14 @@
+angka_list = list(map(int, input("Masukkan angka-angka, dipisahkan dengan spasi: ").split()))
+
+jumlah_total = sum(angka_list)
+
+rata_rata = jumlah_total / len(angka_list)
+
+nilai_terbesar = max(angka_list)
+nilai_terkecil = min(angka_list)
+print("\n Hasil Perhitungan Statistik ")
+print(f" Angka yang dimasukkan    : {angka_list}")
+print(f" Jumlah total             : {jumlah_total}")
+print(f" Nilai rata-rata         : {rata_rata:.2f}")
+print(f" Nilai terbesar          : {nilai_terbesar}")
+print(f" Nilai terkecil          : {nilai_terkecil}")
